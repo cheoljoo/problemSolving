1485f0b0b5ba66a6dc952239f58d0b9ad75aa737
Charles.Lee <cheoljoo@gmail.com>
2019-04-28 14:09:49 +0900

    codejam2019 : C-#3 : long long int

--- ps/codejam2019/3.cpp
+++ ps/codejam2019/3.cpp
@@ -14,68 +14,20 @@ int C;    // case size
 int A;    // A size
 int B;      // B size
 
-vector<int> a;
-vector<int> b;
-
-int startIndex;
-
-int find_near(int e,int& result)
-{
-    int si = startIndex;
-    int se = b.size() -1;;
-    int mid;
-    while(si < b.size()){
-        mid =  (si + se) /2;
-        cout << "si:" << si << ",se:" << se << ",mid:" << mid << ",e:" << e << endl;
-        cout << "a:";
-        for(auto n : a){
-            cout << " " << n;
-        }
-        cout << endl;
-        cout << "b:";
-        for(auto n : b){
-            cout << " " << n;
-        }
-        cout << endl;
-        if(b[si] == e){ 
-            startIndex = si;
-            break;
-        }
-        if(b[si] > e){
-            startIndex = si;
-            break;
-        }
-        if(si == se){
-            startIndex = si;
-            break;
-        }
-        if(si == mid){
-            si++;
-            startIndex = si;
-            break;
-        }
-
-        if(b[mid] > e){
-            si++;
-            se = mid;
-        }
-        else {
-            si = mid;
-        }
-    }
-    cout << "result index:" << si << endl;
-    return startIndex;
-}
+vector<long long int> a;
+set<long long int> b;
 
 int main(int argc,char *argv[])
 {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int v;
-    int idx;
-    int sum;
-    int diff1,diff2;
-    int near;
+    long long int v;
+    std::set<long long int>::iterator itlow,itup;
+
+    long long int sum;
+    long long int diff1,diff2;
+    long long int p1,p2;
+    long long int near;
     
     cin >> C;
 	for(int cnt = 0 ; cnt < C ; cnt++){
@@ -86,26 +38,29 @@ int main(int argc,char *argv[])
         }
         for(int i =0;i<B;i++){
             cin >> v;
-            b.push_back(v);
+            b.insert(v);
         }
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-        int result=0;
-        startIndex = 0;
+        //std::cout << "b contains:";
+        //for (std::set<int>::iterator it=b.begin(); it!=b.end(); ++it)
+            //std::cout << ' ' << *it;
+        //std::cout << '\n';
         sum = 0;
         for(auto e : a){
-            idx = find_near(e,result);
-            if(idx == 0){
-                cout << "near : " << b[idx] << endl;
-                sum += b[idx];
-            } else if(idx == b.size()){
-                cout << "near : " << b[idx-1] << endl;
-                sum += b[idx -1];
+            itlow=b.lower_bound (e);
+            if((e == *itlow) || (itlow == b.begin()) ){
+                //cout << "e:" << e << " ,near : " << *itlow << endl;
+                sum += (*itlow);
+            } else if(itlow == b.end()){
+                itlow--;
+                //cout << "e:" << e << " ,near : " << *itlow << endl;
+                sum += (*itlow);
             } else {
-                diff1 = e - b[idx-1];
-                diff2 = b[idx] - e; 
-                near = (diff1 <= diff2)? b[idx-1] : b[idx];
-                cout << "near : " << near << endl;
+                p2 = *itlow;
+                p1 = *(--itlow);
+                diff1 = e-p1;
+                diff2 = p2-e; 
+                near = (diff1 <= diff2)? p1 : p2;
+                //cout << "e:" << e << " ,near : " << near << endl;
                 sum += near;
             }
         }
--- ps/codejam2019/temp/set1.cpp
+++ ps/codejam2019/temp/set1.cpp
@@ -10,14 +10,47 @@ int main ()
 
   for (int i=1; i<10; i++) myset.insert(i*10); // 10 20 30 40 50 60 70 80 90
 
+  itlow=myset.lower_bound (5);                //       ^
+  if(itlow == myset.begin()) cout << "begin ";
+  std::cout << "5 itlow:" << *itlow << endl;
+  itup=myset.upper_bound (5);                 //                   ^
+  std::cout << "5 itup:" << *itup << endl;
+  itlow=myset.lower_bound (10);                //       ^
+  std::cout << "10 itlow:" << *itlow << endl;
+  itup=myset.upper_bound (10);                 //                   ^
+  std::cout << "10 itup:" << *itup << endl;
+  itlow=myset.lower_bound (18);                //       ^
+  std::cout << "18 itlow:" << *itlow << endl;
+  itup=myset.upper_bound (18);                 //                   ^
+  std::cout << "18 itup:" << *itup << endl;
   itlow=myset.lower_bound (30);                //       ^
   std::cout << "30 itlow:" << *itlow << endl;
+  itup=myset.upper_bound (30);                 //                   ^
+  std::cout << "30 itup:" << *itup << endl;
   itlow=myset.lower_bound (31);                //       ^
   std::cout << "31 itlow:" << *itlow << endl;
+  itup=myset.upper_bound (31);                 //                   ^
+  std::cout << "31 itup:" << *itup << endl;
+  itlow=myset.lower_bound (60);                //       ^
+  std::cout << "60 itlow:" << *itlow << endl;
   itup=myset.upper_bound (60);                 //                   ^
   std::cout << "60 itup:" << *itup << endl;
+  itlow=myset.lower_bound (65);                //       ^
+  std::cout << "65 itlow:" << *itlow << endl;
   itup=myset.upper_bound (65);                 //                   ^
   std::cout << "65 itup:" << *itup << endl;
+  itlow=myset.lower_bound (90);                //       ^
+  if(itlow == myset.end()){ cout << "end ";  --itlow;}
+  std::cout << "90 itlow:" << *itlow << endl;
+  itup=myset.upper_bound (90);                 //                   ^
+  if(itup == myset.end()){ cout<< "end ";  --itup;}
+  std::cout << "90 itup:" << *itup << endl;
+  itlow=myset.lower_bound (95);                //       ^
+  if(itlow == myset.end()){ cout << "end ";  --itlow;}
+  std::cout << "95 itlow:" << *itlow << endl;
+  itup=myset.upper_bound (95);                 //                   ^
+  if(itup == myset.end()){ cout<< "end ";  --itup;}
+  std::cout << "95 itup:" << *itup << endl;
 
   myset.erase(itlow,itup);                     // 10 20 70 80 90
 
