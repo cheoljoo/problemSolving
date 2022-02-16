ef4ab5cdf00a199529674b2c06f638e3d67811e6
Charles.Lee <cheoljoo@gmail.com>
2022-02-16 14:29:43 +0900

    heap

--- leetcode/strangePrinter.py
+++ leetcode/strangePrinter.py
@@ -7,12 +7,32 @@ import math
 import random
 
 class Solution:
+    def __init__(self):
+        self.memo = {}
     def strangePrinter(self, s: str) -> int:
         s = "".join(ch for i, ch in enumerate(s) if i == 0 or s[i-1] != ch)
-        print(s)
         
+        return self.dp(0, len(s)-1, s)
+    
+    def dp(self, start, end, s):
+        # print count for [start, end], end exclusive
+        if (start, end) in self.memo:
+            return self.memo[(start, end)]
+        if start == end:
+            return 1
+        # 1st and others
+        # from 2nd , remove it ( before + after  count) if it is same with start
+        ans = 1 + self.dp(start + 1, end, s) 
         
-        return 2
+        for i in range(start + 1, end+1):
+            # skip the element when it's same as the start
+            if s[start] == s[i]:
+                if i == end:
+                    ans = min(ans,self.dp(start, i-1, s))
+                else :
+                    ans = min(ans, self.dp(start, i-1, s) + self.dp(i + 1, end, s))
+        self.memo[(start, end)] = ans
+        return ans
 
 def run(s,expect):
     ismatch = Solution()
