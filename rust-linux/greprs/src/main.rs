d18cd2228168edcce014a8b5cb57deea1b1b5dbb
charles.lee <cheoljoo.lee@lge.com>
2022-04-23 14:48:40 +0900

    rust: ## 12.2. 테스트 조직화 , # 13. I/O 프로젝트: 커맨드 라인 프로그램 만들기 (예제)

---
+++ rust-linux/greprs/src/main.rs
@@ -0,0 +1,34 @@
+extern crate greprs;   // lib.rs
+use greprs::Config;
+
+use std::env;
+//use std::fs::File;
+//use std::io::prelude::*;
+use std::process;
+//use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = greprs::run(config){
+        println!("Application error: {}",e);
+        process::exit(1);
+    }
+}
+
+/*
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+}
+*/
