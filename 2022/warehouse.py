1edcaabfc68f94e8380ba61a7d441dae02662b20
charles.lee <cheoljoo.lee@lge.com>
2022-07-01 13:45:05 +0900

    2022 : # 11. warehouse - 창고

--- 2022/warehouse.py
+++ 2022/warehouse.py
@@ -31,15 +31,51 @@ import math
 # warehouse.py : https://github.com/cheoljoo/problemSolving/tree/master/leetcode
 
 class Solution:
-    def warehouse(self, n: int) -> List[int]:
-        r = []
-        for i in range(n+1):
-            count = 0
-            while i:
-                i &= i - 1
-                count += 1
-            r.append(count)
-        return r
+    def warehouse(self, n: List[int]) -> List[int]:
+        sm = sum(n)
+        if sm % len(n) != 0:
+            return -1
+        t = sm // len(n)  # equality target
+        # ans = math.inf
+        # 2개의 heapq를 사용한다. 
+        # 1개는 t 보다 작은 것들로 큰 순서로 sort (커야 한번에 더 많이 가져올수 있음)
+        #    추가되어야 한 값을 아래의 큰 값들에서 찾아서, 제일 큰 차이값은 제일 큰 칸에서 뽑으면 문제가 없을듯 하긴함.
+        # 1개는 t 보다 큰 값들로 작은 값에서부터 가져오게 하면 된다.  여기서 나누어 사용하는 것은 아무 제약이 없기 때문이다.
+        lessQ = []
+        greaterQ = []
+        for v in n:
+            if v < t:
+                heapq.heappush(lessQ,-v)
+            elif v > t:
+                heapq.heappush(greaterQ,v)
+        count = 0
+        print("lessQ:",lessQ,"greaterQ:",greaterQ,"target:",t)
+        while lessQ :
+            a = heapq.heappop(lessQ) * -1
+            move = a // 2
+            if a + move >= t:
+                move = t - a
+
+            b = heapq.heappop(greaterQ)
+            print("a,b:",a,b,"move:",move,"count:",count)
+            if b > move:
+                b -= move
+                if b != t:
+                    heapq.heappush(greaterQ,b)
+            elif b < move:
+                move -= b
+            if a+move != t:
+                heapq.heappush(lessQ,(a+move)*-1)
+            count += 1
+            print("lessQ:",lessQ,"greaterQ:",greaterQ,"move:",move,"count:",count)
+            if count > 20:
+                break
+            
+        return count
+                    
+                    
+
+            
 
            
 def run(s,expect):
@@ -71,4 +107,4 @@ if (__name__ == "__main__"):
 
     print('warehouse problem :')
 
-    run(8,[3,3])
+    run([4,8,2,10],4)
